8b58a543b603f63e676d961329ddc5cbce5e49ea
innakhabirova <iikhabi@mail.ru>
2022-04-04 19:36:53 +0300

    junit4 was added

--- src/main/java/ru/netology/cashbackhacker/CashbackHackService.java
+++ src/main/java/ru/netology/cashbackhacker/CashbackHackService.java
@@ -1,10 +1,10 @@
 package ru.netology.cashbackhacker;
 
 public class CashbackHackService {
-private final int boundary=1000;
+    private final int boundary = 1000;
 
-public int remain(int amount){
-    return boundary-amount%boundary;
-}
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
 
 }
--- src/test/java/ru/netology/cashbackhacker/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/cashbackhacker/CashbackHackServiceTest.java
@@ -1,11 +1,11 @@
 package ru.netology.cashbackhacker;
 
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
     public void shouldCalcRemain() {
@@ -14,8 +14,16 @@ class CashbackHackServiceTest {
         int expected = 150;
         int actual = cashbackHackService.remain(amount);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldReturnRemain0For1000Amount() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
 
+        assertEquals(expected, actual);
+    }
 }
